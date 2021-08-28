1f5013e1f7c1c905d3f439f5419d4dd678e37cd9
ganesh-19 <ganeshchandranb@gmail.com>
2021-08-28 12:09:36 +0530

    commit-pick

--- one.go
+++ one.go
@@ -6,18 +6,9 @@ func calc(){
 	fmt.Println("This is another function")
 }
 
-func function3(){
-	fmt.Println("This is function3")
-}
-func function4(){
-	fmt.Println("This is function4")
-}
-
 func main(){
 	name := "Max"
 	fmt.Println("Hello", name)
 	fmt.Println("Welcome")
 	calc()
-	function3()
-	function4()
 }
