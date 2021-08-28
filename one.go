11313ca4832faead368f9b6098930e68f394a929
ganesh-19 <ganeshchandranb@gmail.com>
2021-08-28 12:10:00 +0530

    commit-7

--- one.go
+++ one.go
@@ -2,14 +2,9 @@ package main
 
 import "fmt"
 
-func calc(){
-	fmt.Println("This is another function")
-}
-
 
 func main(){
 	name := "Max"
 	fmt.Println("Hello", name)
 	fmt.Println("Welcome")
-	calc()
 }
